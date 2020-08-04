c762506a79ebbae5608e4f179a2d04a10c53c874
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-04 09:35:57 -0500

    JM Use history replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    //this replace my navigation if i want save my navigation should use push no replace
+    history.replace("/");
+  };
   return (
-    <div>
+    <div className="container mt-5">
       <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Ingresar
+      </button>
     </div>
   );
 };

7cf3d7de83ce7ccb41f1aac03ea272195e9fa258
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-04 09:29:43 -0500

    JM Implement more routers

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "../components/ui/Navbar";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
   return (
     <Router>
+      {/* Aqui puedo colocar estilos que se repitan en toda la App */}
       <div>
-        <Navbar />
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/" component={MarvelScreen} />
+          <Route path="/" component={DashboardRoutes} />
         </Switch>
       </div>
     </Router>

73e37fc17a68d8945ba6c943079ce1dd6aaf0682
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-06 16:05:32 -0500

    JM Public route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoute } from "./PrivateRoute";
 import { AuthContext } from "../auth/AuthContext";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   const { user } = useContext(AuthContext);
@@ -12,7 +13,12 @@ export const AppRouter = () => {
       {/* Aqui puedo colocar estilos que se repitan en toda la App */}
       <div>
         <Switch>
-          <Route exact path="/login" component={LoginScreen} />
+          <PublicRoute
+            exact
+            path="/login"
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
           <PrivateRoute
             path="/"
             component={DashboardRoutes}

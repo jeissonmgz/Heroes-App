6b2a4a8de67ebf33cc86dc84c9c8bcb837aba08c
jeisson-gml <jeissonm@gmlsoftware.com>
2020-08-06 13:33:09 -0500

    JM useHistory

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { AuthContext } from "./auth/AuthContext";
 import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
@@ -7,8 +7,15 @@ const init = () => {
   return JSON.parse(localStorage.getItem("user")) || { logged: false };
 };
 
+const save = (user) => {
+  return localStorage.setItem("user", JSON.stringify(user));
+};
+
 export const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
+  useEffect(() => {
+    save(user);
+  }, [user]);
   return (
     <AuthContext.Provider value={{ user, dispatch }}>
       <AppRouter />
